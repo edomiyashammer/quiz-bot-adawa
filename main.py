4e521e9ca28ed918fa23359c1e8c585ff164d244
edomiyashammer <edomiashammer@gmail.com>
2023-04-04 18:44:40 -0400

    first commit

--- main.py
+++ main.py
@@ -1,70 +1,14 @@
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+import telebot
 
-updater = Updater("6006311344:AAFpHrQxkGXuRANJAo-MCQxJIL992DJmwRA",
-				use_context=True)
+API_KEY = "6006311344:AAFpHrQxkGXuRANJAo-MCQxJIL992DJmwRA"
+bot = telebot.TeleBot(API_KEY)
 
 
-def start(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Hello sir, Welcome to the Bot.Please write\
-		 to see the commands available.")
 
-def help(update: Update, context: CallbackContext):
-	update.message.reply_text("""Available Commands :-
-	/youtube - To get the youtube URL
-	/linkedin - To get the LinkedIn profile URL
-	/gmail - To get gmail URL
-	/geeks - To get the GeeksforGeeks URL""")
+@bot.message_handler(commands=['start'])
+def greet(message):
+    bot.reply_to(message, "this is the new telegram bot that created to help")
 
 
-def gmail_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Your gmail link here (I am not\
-		giving mine one for security reasons)")
 
-
-def youtube_url(update: Update, context: CallbackContext):
-	update.message.reply_text("Youtube Link =>\
-	https://www.youtube.com/")
-
-
-def linkedIn_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"LinkedIn URL => \
-		https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
-
-
-def geeks_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"GeeksforGeeks URL => https://www.geeksforgeeks.org/")
-
-
-def unknown(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Sorry '%s' is not a valid command" % update.message.text)
-
-
-def unknown_text(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Sorry I can't recognize you , you said '%s'" % update.message.text)
-
-
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
-updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
-updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(
-	Filters.command, unknown)) # Filters out unknown commands
-
-# Filters out unknown messages.
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-
-updater.start_polling()
+bot.polling()
