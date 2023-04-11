5915c3950fda51629a0ed8668844e9a1cc3672b2
edomiyashammer <edomiashammer@gmail.com>
2023-04-12 02:21:57 +0300

    first commit

--- main.py
+++ main.py
@@ -1,137 +1,155 @@
 import logging
-import random
+import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters import Text
 from aiogram.types import ParseMode
-from aiogram.utils import markdown
 from aiogram.utils import executor
+from enum import Enum
 
-bot = Bot(token='6006311344:AAFpHrQxkGXuRANJAo-MCQxJIL992DJmwRA')  # Replace with your actual bot token
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and storage
+bot = Bot(token="6006311344:AAFpHrQxkGXuRANJAo-MCQxJIL992DJmwRA")
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-# Quiz question and answer dictionary
-quiz_data = {
-    'What is the capital of France?': 'Paris',
-    'What is the largest mammal?': 'Blue whale',
-    'What is the boiling point of water in Celsius?': '100',
-    'What is the currency of Japan?': 'Yen',
-    'What is the tallest mountain in the world?': 'Mount Everest'
-}
 
-# User's reward coins dictionary
-reward_coins = {}
+# Quiz states
+class QuizState(Enum):
+    Q1 = 1
+    Q2 = 2
+    Q3 = 3
 
-# Quiz command handler
-@dp.message_handler(Command('quiz'))
-async def cmd_quiz(message: types.Message):
-    quiz_question = random.choice(list(quiz_data.keys()))
-    correct_answer = quiz_data[quiz_question]
-
-    # Store the correct answer in FSM context
-    async with FSMContext(storage=storage, chat=message.chat, user=message.from_user) as quiz_context:
-        quiz_context['correct_answer'] = correct_answer
-
-        # Prompt the quiz question
-        await bot.send_message(chat_id=message.chat.id, text=f'Quiz Question:\n\n{quiz_question}')
-        await bot.register_next_step_handler(message, process_quiz_answer)
-
-async def process_quiz_answer(message: types.Message, state: FSMContext):
-    async with state.proxy() as quiz_data:
-        correct_answer = quiz_data['correct_answer']
-        user_answer = message.text
-
-        # Check if the answer is correct
-        if user_answer == correct_answer:
-            # Award 1 coin for correct answer
-            await bot.send_message(chat_id=message.chat.id, text='Correct! You earned 1 coin.')
-            user_id = message.from_user.id
-            if user_id not in reward_coins:
-                reward_coins[user_id] = 0
-            reward_coins[user_id] += 1
-        else:
-            await bot.send_message(chat_id=message.chat.id, text='Wrong! Better luck next time.')
-
-    # Reset the FSM state
-    await state.finish()
-
-# WiFi report command handler
-@dp.message_handler(Command('wifi_report'))
-async def cmd_wifi_report(message: types.Message):
-    # Prompt for serial number
-    await bot.send_message(chat_id=message.chat.id, text='Please enter the serial number:')
-    await bot.register_next_step_handler(message, process_wifi_serial_number)
-
-async def process_wifi_serial_number(message: types.Message):
-    serial_number = message.text
-
-    # Prompt for description
-    await bot.send_message(chat_id=message.chat.id, text='Please enter the description of the Wi-Fi issue:')
-    await bot.register_next_step_handler(message, process_wifi_description, serial_number)
-
-async def process_wifi_description(message: types.Message, state: FSMContext):
-    serial_number = state['serial_number']
-    description = message.text
-
-    await bot.send_message(chat_id=message.chat.id, text='Thank you for reporting the Wi-Fi issue. We will look into it.')
-
-    await state.finish()
-
-# PUK report command handler
-@dp.message_handler(Command('puk_report'))
-async def cmd_puk_report(message: types.Message):
-    # Prompt for SIM card number
-    await bot.send_message(chat_id=message.chat.id, text='Please enter the Phone number:')
-    await bot.register_next_step_handler(message, process_puk_sim_number)
-
-async def process_puk_sim_number(message: types.Message):
-    sim_number = message.text
-
-    # Prompt for description
-    await bot.send_message(chat_id=message.chat.id, text='Please enter the description of the PUK issue:')
-    await bot.register_next_step_handler(message, process_puk_description, sim_number)
-
-async def process_puk_description(message: types.Message, state: FSMContext):
-    sim_number = state['sim_number']
-    description = message.text
-
-    await bot.send_message(chat_id=message.chat.id, text='Thank you for reporting the PUK issue. We will look into it.')
-
-    await state.finish()
 
 # Start command handler
-@dp.message_handler(Command('start'))
+@dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
     """
-    Conversation's entry point
+    Send a welcome message and instructions on how to use the bot
     """
-    # Check if user is already registered
-    user_id = message.from_user.id
-    if user_id in reward_coins:
-        await bot.send_message(chat_id=message.chat.id, text='Welcome back! You have earned {} coins.'.format(reward_coins[user_id]))
-    else:
-        await bot.send_message(chat_id=message.chat.id, text='Welcome! You have not earned any coins yet.')
+    await bot.send_message(chat_id=message.chat.id, text='Welcome to the Multi-Task Bot!\n'
+                                                     'You can use this bot for the following tasks:\n'
+                                                     '- Quiz: Answer quiz questions and win rewards\n'
+                                                     '- WiFi: Report WiFi problems\n'
+                                                     '- PUK: Report PUK problems\n'
+                                                     'Type /quiz to start the quiz, or type /help for instructions.')
+
 
 # Help command handler
-@dp.message_handler(Command('help'))
+@dp.message_handler(commands='help')
 async def cmd_help(message: types.Message):
     """
-    Show help message
+    Send instructions on how to use the bot
     """
-    help_text = "You can control me by sending these commands:\n\n" \
-                "/start - Start the bot and check your coins\n" \
-                "/quiz - Participate in a quiz and earn coins\n" \
-                "/wifi_report - Report Wi-Fi issue and earn coins\n" \
-                "/puk_report - Report PUK issue and earn coins\n" \
-                "/help - Show this help message"
+    await bot.send_message(chat_id=message.chat.id, text='Here are the available commands:\n'
+                                                     '- /quiz: Start the quiz\n'
+                                                     '- /wifi: Report WiFi problems\n'
+                                                     '- /puk: Report PUK problems')
 
-    await bot.send_message(chat_id=message.chat.id, text=help_text, parse_mode=ParseMode.MARKDOWN)
+
+# Quiz command handler
+@dp.message_handler(commands='quiz')
+async def cmd_quiz(message: types.Message):
+    """
+    Start the quiz and reward users for correct answers
+    """
+    # Set initial quiz state
+    await bot.send_message(chat_id=message.chat.id, text='Welcome to the quiz! Answer the following questions:')
+    await bot.send_message(chat_id=message.chat.id, text='What is the capital of France?')
+    await QuizState.Q1.set()
+
+
+# WiFi problem reporting handler
+@dp.message_handler(commands='wifi')
+async def cmd_wifi(message: types.Message):
+    """
+    Report WiFi problems
+    """
+    bot.send_message(chat_id=message.chat.id, text='Please Enter your WiFi Serial:')
+
+    await bot.send_message(chat_id=message.chat.id, text='Please describe your WiFi problem:')
+    # Set WiFi problem reporting state
+    await FSMContext.set_state('wifi_report')
+
+
+# PUK problem reporting handler
+@dp.message_handler(commands='puk')
+async def cmd_puk(message: types.Message):
+    """
+    Report PUK problems
+    """
+    await bot.send_message(chat_id=message.chat.id, text='Please describe your PUK problem:')
+    # Set PUK problem reporting state
+    await FSMContext.set_state('puk_report')
+
+
+# Answer handler for quiz questions
+@dp.message_handler(lambda message: message.text, state=QuizState.Q1)
+async def process_quiz_q1(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        # Process answer for Q1
+        if message.text == 'Paris':
+            # Correct answer
+            await bot.send_message(chat_id=message.chat.id, text='Correct answer! Next question:')
+            await bot.send_message(chat_id=message.chat.id, text='What is the currency of Japan?')
+            data['quiz_state'] = QuizState.Q2.value  # Update quiz state to Q2
+# Answer handler for quiz questions
+@dp.message_handler(lambda message: message.text, state=QuizState.Q2)
+async def process_quiz_q2(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        # Process answer for Q2
+        if message.text == 'Yen':
+            # Correct answer
+            await bot.send_message(chat_id=message.chat.id, text='Correct answer! Final question:')
+            await bot.send_message(chat_id=message.chat.id, text='What is the tallest mountain in the world?')
+            data['quiz_state'] = QuizState.Q3.value  # Update quiz state to Q3
+        else:
+            # Incorrect answer
+            await bot.send_message(chat_id=message.chat.id, text='Wrong answer. Try again:')
+            await bot.send_message(chat_id=message.chat.id, text='What is the currency of Japan?')
+
+
+@dp.message_handler(lambda message: message.text, state=QuizState.Q3)
+async def process_quiz_q3(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        # Process answer for Q3
+        if message.text == 'Mount Everest':
+            # Correct answer
+            await bot.send_message(chat_id=message.chat.id, text='Congratulations! You answered all questions correctly.')
+            await bot.send_message(chat_id=message.chat.id, text='You have won a reward!')
+        else:
+            # Incorrect answer
+            await bot.send_message(chat_id=message.chat.id, text='Wrong answer. Try again:')
+            await bot.send_message(chat_id=message.chat.id, text='What is the tallest mountain in the world?')
+
+
+# WiFi problem reporting handler
+@dp.message_handler(state='wifi_report')
+async def process_wifi_report(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        # Process WiFi problem report
+        wifi_problem = message.text
+        await bot.send_message(chat_id=message.chat.id, text='Thank you for reporting your WiFi problem:\n'
+                                                             f'{md.quote_html(wifi_problem)}')
+        # Reset state
+        await state.finish()
+
+
+# PUK problem reporting handler
+@dp.message_handler(state='puk_report')
+async def process_puk_report(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        # Process PUK problem report
+        puk_problem = message.text
+        await bot.send_message(chat_id=message.chat.id, text='Thank you for reporting your PUK problem:\n'
+                                                             f'{md.quote_html(puk_problem)}')
+        # Reset state
+        await state.finish()
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
-    # Start the bot
     executor.start_polling(dp, skip_updates=True)
